a459e643f2f99a821c5997f097f746c645dfee70
Daniil Gorbachev <Daniil1938@yandex.ru>
2023-05-08 18:10:00 +0300

    first version: simple interface + text output without diagrams

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, QLabel, QLineEdit, QPushButton
+from PyQt5.QtCore import *
+from PyQt5.QtChart import QChart, QChartView, QPieSeries, QPieSlice
+from PyQt5.QtGui import *
+from SentimentModel import SentimentModel
+from math import ceil
 
+
+def create_pie_chart(data):
+    # Создание серии диаграммы
+    series = QPieSeries()
+
+    # Добавление данных в серию
+    for label, (value, color) in data.items():
+        _slice = QPieSlice(label, value)
+        _slice.setBrush(color)
+        series.append(_slice)
+
+    # Создание диаграммы
+    chart = QChart()
+    chart.addSeries(series)
+    chart.setTitle("Кольцевая диаграмма")
+
+    # Создание виджета для отображения диаграммы
+    chart_view = QChartView(chart)
+    #chart_view.setRenderHint(QChartView.Antialiasing)
+
+    return chart_view
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        # Данные для кольцевой диаграммы
         self.initUI()
 
     def initUI(self):
@@ -25,17 +51,38 @@ class MainWindow(QWidget):
         self.analyze_button.clicked.connect(self.analyze_post)
 
         self.result_label = QLabel(self)
-        self.result_label.setGeometry(20, 120, 360, 30)
+        self.result_label.setGeometry(20, 120, 360, 60)
         self.result_label.setAlignment(Qt.AlignCenter)
+
+        #layout = QVBoxLayout()
+        #layout.addWidget(self.pie_chart)
+
+        #self.setLayout(layout)
 
         self.show()
 
     def analyze_post(self):
         post_text = self.post_edit.text()
 
-        ## Тут будет код для анализа поста
+        sent_model = SentimentModel()
+        predictions = sent_model.predict(post_text)
+        results = [float('{:.3f}'.format(predictions[_type]*100)) for _type in ["NEUTRAL", "POSITIVE", "NEGATIVE"]]
+        pred_str = f"NEUTRAL: {results[0]}%\n" \
+                   f"POSITIVE: {results[1]}%\n" \
+                   f"NEGATIVE: {results[2]}%"
 
-        self.result_label.setText("Уровень агрессии: 50%")  # Потом изменю
+        #self.pie_chart = create_pie_chart({"NEUTRAL": (results[0], QtGui.QColor("#F5D572")),
+        #                                   "POSITIVE": (results[1], QtGui.QColor("#32CD32")),
+        #                                   "NEGATIVE": (results[2], QtGui.QColor("#FF3E3E"))})
+
+        #layout = QVBoxLayout()
+        #layout.addWidget(self.pie_chart)
+
+        #self.setLayout(layout)
+
+        #self.show()
+
+        self.result_label.setText(pred_str)
 
 
 def main():
