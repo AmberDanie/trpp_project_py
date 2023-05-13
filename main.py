7f0b8b70a3430b31fddc4b60f176adc9d2756fee
Daniil Gorbachev <Daniil1938@yandex.ru>
2023-05-13 23:14:22 +0300

    Fourth version v0.5: last results updated

--- main.py
+++ main.py
@@ -128,28 +128,81 @@ class Screen3(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.layout = QVBoxLayout()
+        self.top_layout = QVBoxLayout(self)
+        self.setLayout(self.top_layout)
+        self.info_widget = QWidget()
+        self.info_layout = QVBoxLayout()
+        self.info_layout.addStretch()
+        self.info_widget.setLayout(self.info_layout)
+
+        self.scrollable_area = QScrollArea()
+        self.scrollable_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scrollable_area.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scrollable_area.setWidgetResizable(True)
+        self.scrollable_area.setWidget(self.info_widget)
+
+        self.returnButton = QPushButton("Return")
+        self.returnButton.clicked.connect(self.go_to_main_screen)
+        self.top_layout.addWidget(self.scrollable_area)
+        self.top_layout.addWidget(self.returnButton)
+
+        percentage_text = QLabel("Percentage")
+        percentage_text.setAlignment(Qt.AlignCenter)
+        percentage_text.set
+        diagram_text = QLabel("Diagram")
+        diagram_text.setAlignment(Qt.AlignCenter)
+        inputed_text = QLabel("Inputed text")
+        inputed_text.setAlignment(Qt.AlignCenter)
+
+        layout = QHBoxLayout()
+        layout.addWidget(percentage_text)
+        layout.addWidget(diagram_text)
+        layout.addWidget(inputed_text)
+
+        self.info_layout.addLayout(layout)
+
+        layout.setAlignment(Qt.AlignTop)
+
+        self.setLayout(self.top_layout)
+
+
     def get_last_res(self):
         query = "SELECT ROW_NUMBER() over() as number,* FROM story ORDER BY number DESC"
         curs.execute(query)
         result_table = curs.fetchall()
+        query = "DELETE FROM story *"
+        curs.execute(query)
+        conn.commit()
         for row in result_table:
-            line_layout = QHBoxLayout()
+            layout = QHBoxLayout()
             row_pie = create_pie_chart({"POSITIVE": (row[3], QtGui.QColor("#32CD32")),
                                         "NEUTRAL": (row[4], QtGui.QColor("#F5D572")),
                                         "NEGATIVE": (row[2], QtGui.QColor("#FF3E3E"))}, listBool=False)
-
+            print(3)
+            row_pie.setFixedSize(300, 300)
             pred_str = f"POSITIVE: {row[3]}%\nNEUTRAL: {row[4]}%\nNEGATIVE: {row[2]}%"
-            line_layout.addWidget(row_pie)
             text_label = QLabel()
             text_label.setAlignment(Qt.AlignCenter)
             stat_label = QLabel(pred_str)
             stat_label.setAlignment(Qt.AlignCenter)
-            text_label.setText(row[1])
-            line_layout.addWidget(stat_label)
-            line_layout.addWidget(text_label)
-            self.layout.addLayout(line_layout)
-        self.setLayout(self.layout)
+            stat_label.setStyleSheet('width: 25%; text-align="center";')
+            text_label.setStyleSheet("width: 25%; text-align='center':")
+            print(2)
+            if row[1]:
+                text_label.setText(row[1])
+            else:
+                text_label.setText("<<empty>>")
+            print(1)
+
+            layout.addWidget(stat_label)
+            layout.addWidget(row_pie)
+            layout.addWidget(text_label)
+
+            self.info_layout.addLayout(layout)
+
+
+    def go_to_main_screen(self):
+        widget.setCurrentIndex(widget.currentIndex() - 2)
 
 
 
